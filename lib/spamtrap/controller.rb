3dda042294e07ee3afb0a66e426c744a23e02625
Cedric Howe <cedric@freezerbox.com>
2010-10-26 17:48:10 -0400

    Fixed use of symbol for single mixin action.

--- lib/spamtrap/controller.rb
+++ lib/spamtrap/controller.rb
@@ -6,8 +6,9 @@ module Spamtrap
   
   module ActsAsMethods
     def spamtrap(actions, parameter='spamtrap')
+      actions = [actions] unless actions.is_a?(Array)
       raise 'Spamtrap must have actions defined.' if actions.empty?
-      before_filter(:only => actions.is_a?(Array) ? actions : [actions]) do |controller|
+      before_filter(:only => actions) do |controller|
         controller.instance_eval do
           if params[parameter.to_s].present?
             Rails.logger.warn "Spamtrap triggered by #{request.remote_ip}."
