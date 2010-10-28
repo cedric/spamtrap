3c62c0a469d6f67600e1a7690b5552147c1e7523
Cedric Howe <cedric@freezerbox.com>
2010-10-28 19:34:51 -0400

    Added test files.

---
+++ test/controller_test.rb
@@ -0,0 +1,16 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+class ControllerTest < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_foobar
+    assert true
+    assert_equal "hello", "hello"
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,12 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+$: << File.join(File.dirname(__FILE__))
+
+require 'rubygems'
+require 'test/unit'
+# require 'active_support'
+require 'action_controller'
+# require 'action_controller/test_case'
+# require 'action_view'
+# require 'active_record'
+# require 'ruby-debug'
+require 'spamtrap'
