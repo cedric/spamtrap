b7382776a755a32e56d18d3d3cc91f7f143c5e18
Cedric Howe <cedric@kickstarter.com>
2013-08-23 10:30:59 -0700

    Remove setup and teardown.

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -2,12 +2,6 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 
 class ControllerTest < Test::Unit::TestCase
 
-  def setup
-  end
-
-  def teardown
-  end
-
   def test_foobar
     assert true
     assert_equal "hello", "hello"
